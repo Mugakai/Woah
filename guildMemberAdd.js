97eeee9a52c3100196f7f3e0650b494d54ce260c
Mugakai <74754507+Mugakai@users.noreply.github.com>
2020-11-22 02:50:44 -0300

    Add files via upload

---
+++ guildMemberAdd.js
@@ -0,0 +1,20 @@
+module.exports = (client, member) => {
+    client.channels.cache.get("774274642520965182").send(`**:partying_face: | Seja bem vindo, `+"`"+member.username+"`"+`! Nós do ` + "`Rubiky`" + ` esperamos que se divirta durante sua estadia em nosso servidor!**`);
+    member.send({embed:{
+        color: "RANDOM",
+        title: "🏓 | bem-vindo ao `Rubiky`! Espero que se divirta no servidor!",
+        fields: [
+            {
+                name: "😇 | quer ajudar nosso servidor?",
+                value: "para isso, que tal dar bump? É bem simples, apenas vá em um dos nossos chats de comandos e escreva: `!d bump`!",
+                inline: true
+            },
+
+            {
+                name: "🤖 | para saber meus comandos:",
+                value: "me chame! Como fazer isso? Bem simples, apenas diga: `Woah!`.",
+                inline: true
+            }
+        ]
+    }});
+}
