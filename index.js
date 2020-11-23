a2b007841bc5c8940c1aa023bac573f6cac4fbd1
Mugakai <74754507+Mugakai@users.noreply.github.com>
2020-11-23 05:30:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,38 +9,43 @@ const Canvas = require('canvas');
 
 client.config = config;
 
-if (dados.t !== "MESSAGE_REACTION_ADD" && dados.t !== "MESSAGE_REACTION_REMOVE") return
-if (dados.d.message_id != "780169693406429235") return
+client.on('raw', async dados => {
+    //#region Reaction Role
+    if (dados.t !== "MESSAGE_REACTION_ADD" && dados.t !== "MESSAGE_REACTION_REMOVE") return
+    if (dados.d.message_id != "780169693406429235") return
 
-let servidor = await client.guilds.cache.get("774274642520965180")
-let membro = await servidor.members.cache.get(dados.d.user_id);
+    let servidor = await client.guilds.cache.get("774274642520965180")
+    let membro = await servidor.members.cache.get(dados.d.user_id);
 
-let cafet = servidor.roles.cache.get("774663123775914015"),
-    inkei = servidor.roles.cache.get("780175676380414002"),
-    mokuzai = servidor.roles.cache.get("780176147681378335");
+    let cafet = servidor.roles.cache.get("774663123775914015"),
+        inkei = servidor.roles.cache.get("780175676380414002"),
+        mokuzai = servidor.roles.cache.get("780176147681378335");
         
-if (dados.t === "MESSAGE_REACTION_ADD") {
-   if (dados.d.emoji.id === "774669881996083220") {
-      if (membro.roles.cache.has(cafet)) return
-      membro.roles.add(cafet);
-   } else if (dados.d.emoji.id === "774665913559023738") {
-      if (membro.roles.cache.has(inkei)) return
-      membro.roles.add(inkei);
-   } else if (dados.d.emoji.id === "775031385542885406") {
-      if (membro.roles.cache.has(inkei)) return
-      membro.roles.add(mokuzai);
-   }
-}
+    if (dados.t === "MESSAGE_REACTION_ADD") {
+        if (dados.d.emoji.id === "774669881996083220") {
+            if (membro.roles.cache.has(cafet)) return
+            membro.roles.add(cafet);
+        } else if (dados.d.emoji.id === "774665913559023738") {
+            if (membro.roles.cache.has(inkei)) return
+            membro.roles.add(inkei);
+        } else if (dados.d.emoji.id === "775031385542885406") {
+            if (membro.roles.cache.has(inkei)) return
+            membro.roles.add(mokuzai);
+        }
+    }
 
-if (dados.t === "MESSAGE_REACTION_REMOVE") {
-   if (dados.d.emoji.id === "774669881996083220") {
-        membro.roles.remove(cafet);
-   } else if (dados.d.emoji.id === "774665913559023738") {
-        membro.roles.remove(inkei);
-   } else if (dados.d.emoji.id === "775031385542885406") {
-        membro.roles.remove(mokuzai);
-   }
-}
+    if (dados.t === "MESSAGE_REACTION_REMOVE") {
+        if (dados.d.emoji.id === "774669881996083220") {
+            membro.roles.remove(cafet);
+        } else if (dados.d.emoji.id === "774665913559023738") {
+            membro.roles.remove(inkei);
+        } else if (dados.d.emoji.id === "775031385542885406") {
+            membro.roles.remove(mokuzai);
+        }
+    }
+    //#endregion
+});
+
 
 client.on('message', message => {
     if (message.content.toLowerCase() === "woah!") {
